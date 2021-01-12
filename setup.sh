9f3a6cfdca070a3e4d876543b9046dbe01e00f2d
codemasterv <codemasterv@gmail.com>
2021-01-12 15:18:12 -0700

    fixed

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 !#/bin/sh
 
-sudo apt install apache2 -y&&sudo systemctl enable apache2&&cd ~&&sudo rm -fR /var/www/html&&sudo rm -fR ~/html&&sudo git clone https://github.com/codemasterv/AlAzif_Lazy_Clone.git&&sudo mv AlAzif_Lazy_Clone html&&sudo chmod 755 -R html&&sudo mv -f html /var/www/&&echo rebooting now press enter&&read&&sudo reboot now
+sudo apt install apache2 -y&&sudo systemctl enable apache2&&cd ~&&sudo rm -fR /var/www/html&&sudo rm -fR ~/html&&sudo rm -fR ~/AlAzif_Lazy_Clone&&sudo git clone https://github.com/codemasterv/AlAzif_Lazy_Clone.git&&sudo mv AlAzif_Lazy_Clone html&&sudo chmod 755 -R html&&sudo mv -f html /var/www/&&echo rebooting now press enter&&read&&sudo reboot now
